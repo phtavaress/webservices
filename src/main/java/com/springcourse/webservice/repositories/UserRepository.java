cd4c5a81713dfa41a55ce74c7cb34d75c4349dae
Pedro Tavares <ph_tavares@hotmail.com>
2022-04-07 10:05:07 -0300

    Service layer, component registration

--- src/main/java/com/springcourse/webservice/repositories/UserRepository.java
+++ src/main/java/com/springcourse/webservice/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.springcourse.webservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springcourse.webservice.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
