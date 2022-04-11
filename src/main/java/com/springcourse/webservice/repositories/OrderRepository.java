aaac662d4691c51e65bc992636098aa9df9b5116
Pedro Tavares <ph_tavares@hotmail.com>
2022-04-11 19:14:57 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/springcourse/webservice/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.springcourse.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.webservice.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
